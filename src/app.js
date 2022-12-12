f3ebcc41003575e94502b3e887288536cad951a3
ericlinsd <ericlinswe@gmail.com>
2022-12-11 16:34:37 -0800

    restructured directory

--- src/app.js
+++ src/app.js
@@ -31,8 +31,7 @@ const server = http.createServer((req, res) => {
       res.end(resBody);
       return;
     }
-    /* Serving Static Assets
-
+    // Serving Static Assets
     const ext = path.extname(req.url);
     if (req.method === "GET" && ext) {
       try {
@@ -55,7 +54,6 @@ const server = http.createServer((req, res) => {
         );
       }
     }
-    */
     // Page Not Found
     res.statusCode = 404;
     res.setHeader("Content-Type", "text/plain");
@@ -65,6 +63,6 @@ const server = http.createServer((req, res) => {
   });
 });
 // Set the port to 5000
-const port = 6000;
+const port = 5000;
 // Tell the port to listen for requests on localhost:5000
 server.listen(port, () => console.log("Server is running on port", port));
