9588f44d7d311c64cea75ec2097d4967a1cc0413
ericlinsd <ericlinswe@gmail.com>
2022-12-13 22:37:57 -0800

    project directory file changes

--- src/main/index.js
+++ src/main/index.js
@@ -1,12 +1,3 @@
 // console log all paragraphs within the first div element
 export const doc = () => {
-    let elementNodes = 0;
-    let otherNodes = 0;
-    const firstDiv = document.body.children.item(1);
-    const nodeListOfFirstDiv = firstDiv.childNodes;
-    nodeListOfFirstDiv.forEach((node) => {
-        node instanceof Element ? elementNodes += 1 : otherNodes += 1;
-    });
-    console.log(`number of element nodes: ${elementNodes}, number of other nodes: ${otherNodes}`);
-    console.log(nodeListOfFirstDiv);
 };
